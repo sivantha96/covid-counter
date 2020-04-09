d4fa7d2d1a71a3d474ff2b3d7339bd7268cc92df
Sivantha Paranavithana <madusham3@gmail.com>
2020-04-10 01:46:55 +0530

    Add styles to figure page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,10 +9,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatRadioModule } from "@angular/material/radio";
-import { FormsModule } from '@angular/forms'
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FigureComponent } from './component/figure/figure.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {
+  FigureComponent,
+  FigureComponentDialog,
+  FigureComponentHeadDialog,
+} from "./component/figure/figure.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatRippleModule } from "@angular/material/core";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
@@ -20,6 +27,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     HeaderComponent,
     LandingComponent,
     FigureComponent,
+    FigureComponentDialog,
+    FigureComponentHeadDialog,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +40,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatDialogModule,
+    MatRippleModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/component/figure/figure.component.ts
+++ frontend/src/app/component/figure/figure.component.ts
@@ -1,14 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
+
+export interface DialogData {
+  clickName: string;
+}
 
 @Component({
-  selector: 'app-figure',
-  templateUrl: './figure.component.html',
-  styleUrls: ['./figure.component.css']
+  selector: "app-figure",
+  templateUrl: "./figure.component.html",
+  styleUrls: ["./figure.component.css"],
 })
 export class FigureComponent implements OnInit {
-  showMan:boolean = true
-  constructor() { }
+  showMan: boolean = false;
 
-  ngOnInit(): void {
+  constructor(public dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {}
+
+  handleClick(click) {
+    console.log("clicked");
+    if (click === "head") {
+      const dialogRef = this.dialog.open(FigureComponentHeadDialog, {
+        width: "90vh",
+        height: "80vh",
+        data: { clickName: click },
+        autoFocus: false
+      });
+    } else {
+      const dialogRef = this.dialog.open(FigureComponentDialog, {
+        width: "90vh",
+        height: "80vh",
+        data: { clickName: click },
+      });
+    }
+  }
+}
+
+@Component({
+  selector: "app-figure-dialog",
+  templateUrl: "./figure.component.dialog.html",
+})
+export class FigureComponentDialog {
+  isHead: boolean = false;
+  imageURL: string = "../../../assets/cough.jpg"
+  constructor(
+    public dialogRef: MatDialogRef<FigureComponentDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+}
+
+@Component({
+  selector: "app-figure-head-dialog",
+  templateUrl: "./figure.component.head.dialog.html",
+  styleUrls: ["./figure.component.css"],
+})
+export class FigureComponentHeadDialog {
+  isHead: boolean = false;
+
+  constructor(
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<FigureComponentHeadDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  handleClickHead(click) {
+    const dialogRef = this.dialog.open(FigureComponentDialog, {
+      width: "90vh",
+      height: "80vh",
+      data: { clickName: click },
+    });
+  }
+
+  handleClick(click) {
+    console.log(click);
   }
 }
