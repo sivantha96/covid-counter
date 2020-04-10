609599f6659b0e6477a96c6839f29dbe63b81856
Sivantha Paranavithana <madusham3@gmail.com>
2020-04-10 13:12:59 +0530

    Refactor the files

--- frontend/src/app/component/landing/landing.component.ts
+++ frontend/src/app/component/landing/landing.component.ts
@@ -2,19 +2,15 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 
-
-
 interface IAge {
   value: string;
   viewValue: string;
 }
 
-interface IForign {
+interface IForeign {
   value:boolean,
   viewValue: string;
 }
-
-
 
 @Component({
   selector: 'app-landing',
@@ -22,36 +18,28 @@ interface IForign {
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  
-  Gender: string[] = ['Male', 'Female'];
+  gender: string[] = ['Male', 'Female'];
   ageGroups:IAge[]=[
   {value: 'age-0', viewValue: 'younger than 40'},
   {value: 'age-1', viewValue: 'between 40-60'},
   {value: 'age-2', viewValue: 'older than 60'}]
 
-  forignGroups:IForign[]=[
+  foreignGroups:IForeign[]=[
     {value: true, viewValue: 'Yes'},
     {value: false, viewValue: 'No'},
   ]
 
-  // dataForm = new FormGroup({
-  //   gender: new FormControl('', Validators.required),
-  //   age: new FormControl('', Validators.required),
-  // })
-
   dataForm=new FormGroup({
     info1:new FormGroup({
       noOfFamily: new FormControl('', Validators.required),
-      youForign: new FormControl('', Validators.required),
-      familyForign: new FormControl('', Validators.required),
+      youForeign: new FormControl('', Validators.required),
+      familyForeign: new FormControl('', Validators.required),
     }),
 
     info2:new FormGroup({
       gender: new FormControl('', Validators.required),
       age: new FormControl('', Validators.required),
     })
-    
   })
 
   constructor(private router:Router, activatedRoute:ActivatedRoute) { }
@@ -71,14 +59,11 @@ export class LandingComponent implements OnInit {
 
   }
 
-
-
   numberOnly(event): boolean {
     const charCode = (event.which) ? event.which : event.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57)) {
       return false;
     }
     return true;
-
   }
 }
